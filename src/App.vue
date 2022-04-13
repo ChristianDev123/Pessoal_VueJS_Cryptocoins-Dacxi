e13c10b5a8ffafdb3a1c5cf1d000e1d941c5d933
ChrisDev <christian.lima999@gmail.com>
2022-04-13 00:37:21 -0300

    start development Footer

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,31 @@
     </div>
   </header>
   <main>
-
+    <Footer/>
   </main>
   
 </template>
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@500;700&display=swap');
+  *{
+    font-family: 'Nunito', sans-serif;
+  }
   :root{
     --primary:#216190;
     --line-detail:rgba(33,97,144,0.75);
+    --footerBackground:rgba(204,204,204,0.22);
   }
 </style>
 <script>
   import Header from './components/HeaderBox/HeaderBox.vue';
+  import Footer from './components/Footer/Footer.vue';
   import './assets/tailwind.css';  
-    export default{
+  
+  export default{
       name:'App',
       components:{
-        Header
+        Header,
+        Footer
       }
     };
 </script>
