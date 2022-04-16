0f4d2242360d90a2cef8e4e2e14510327f982c77
ChristianDev123 <83887907+ChristianDev123@users.noreply.github.com>
2022-04-16 18:17:58 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/SPA_Dashboard_Cryptocoins_Dacxi/'
-//     : '/'
-// }
 module.exports = {
-  productionSourceMap: false,  
-  outputDir: './dist',
-  assetsDir: 'static',
-  indexPath: 'templates/index.html'
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/SPA_Dashboard_Cryptocoins_Dacxi/'
+    : '/'
+}
